3b6173de6ba8bac6f6a119ea08788fdd91b23cd1
Matildehdz4811 <24310649@utcalvillo.edu.mx>
2026-05-27 20:32:53 -0600

    b

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-const rutaFrontend = path.join(__dirname, 'frontend/www');
+const rutaFrontend = path.join(__dirname, 'frontend/dist/browser');
 console.log("--- DIAGNÓSTICO DE CARPETAS ---");
 console.log("Directorio actual del servidor:", __dirname);
 console.log("Ruta que estamos buscando:", rutaFrontend);
