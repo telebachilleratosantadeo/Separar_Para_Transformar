e169bc551eb7bae2c699713ed8ddfa77b3a86f23
Matildehdz4811 <24310649@utcalvillo.edu.mx>
2026-05-27 20:30:43 -0600

    b

--- api/server.js
+++ api/server.js
@@ -12,13 +12,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-// La ruta ahora es local, dentro de la misma carpeta
-const rutaFrontend = path.join(__dirname, 'frontend/www');
 
+const rutaFrontend = path.join(__dirname, 'frontend/www');
 console.log("--- DIAGNÓSTICO DE CARPETAS ---");
 console.log("Directorio actual del servidor:", __dirname);
 console.log("Ruta que estamos buscando:", rutaFrontend);
-// Verificamos si existe 'frontend' directamente dentro de la carpeta actual
 console.log("¿Existe la carpeta 'frontend'?", fs.existsSync(path.join(__dirname, 'frontend')));
 console.log("¿Existe la carpeta 'www' dentro de 'frontend'?", fs.existsSync(rutaFrontend));
 console.log("--- FIN DEL DIAGNÓSTICO ---");
