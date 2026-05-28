8fac8a6bb7202b30647a3f08ba0cd406dbdc9d98
Matildehdz4811 <24310649@utcalvillo.edu.mx>
2026-05-27 20:37:53 -0600

    c

--- api/server.js
+++ api/server.js
@@ -12,13 +12,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-const rutaFrontend = path.join(__dirname, 'frontend/dist/browser');
+const rutaFrontend = path.join(__dirname, 'Frontal/www');
 console.log("--- DIAGNÓSTICO DE CARPETAS ---");
 console.log("Directorio actual del servidor:", __dirname);
 console.log("Ruta que estamos buscando:", rutaFrontend);
-console.log("¿Existe la carpeta 'frontend'?", fs.existsSync(path.join(__dirname, 'frontend')));
-console.log("¿Existe la carpeta 'www' dentro de 'frontend'?", fs.existsSync(rutaFrontend));
+console.log("¿Existe la carpeta 'Frontal'?", fs.existsSync(path.join(__dirname, 'Frontal')));
+console.log("¿Existe la carpeta 'www' dentro de 'Frontal'?", fs.existsSync(rutaFrontend));
 console.log("--- FIN DEL DIAGNÓSTICO ---");
 
 app.use(express.static(rutaFrontend));
